bc85d01e349650270ebb520973f35d8e18fd745d
Rune Myrland <rune@skalden.com>
2025-01-01 19:09:55 +0100

    ensure

--- bin/update.sh
+++ bin/update.sh
@@ -8,6 +8,9 @@ rm -rf files*
 git pull
 gradle installDist
 ./build/install/wiki2epub/bin/wiki2epub
+ls -1 files/ | wc -l | grep 439 && sleep 3600 && ./build/install/wiki2epub/bin/wiki2epub
+ls -1 files/ | wc -l | grep 439 && sleep 3600 && ./build/install/wiki2epub/bin/wiki2epub
+ls -1 files/ | wc -l | grep 439 || exit
 bin/epubcheck docs/download/iliaden.epub && git commit -a -m updated && git push
 echo "Finished: $(date)"
 
