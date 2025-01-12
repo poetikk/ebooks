e729b5d123ee36c7605ac447c2ea84bf05f670cd
Rune Myrland <rune@skalden.com>
2025-01-12 11:09:46 +0100

    gdev_main

--- bin/generate.sh
+++ bin/generate.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_DIR/..
 
 echo "Starting: $(date)"
 git pull
+git pull gdev main
 gradle installDist
 ./build/install/wiki2epub/bin/wiki2epub && bin/epubcheck docs/download/iliaden.epub && git commit -a -m updated && git push
 echo "Finished: $(date)"
